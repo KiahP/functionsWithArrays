5d650c470cb8eb5e1f78d24282ce82bd694f1f16
Kiah Pleasant <kiahpleasant@gmail.com>
2021-02-02 08:10:38 -0500

    push to gh

---
+++ app.js
@@ -0,0 +1,16 @@
+var kiahArray = [0,1,2,3,4,5,6,7,8,9];
+
+function myArray (array,value){
+    array.push(value);
+    return array;
+}
+
+function newArray (x) {
+    array.splice(x);
+    return newArray;
+}
+
+function sliceArray (x){
+    array.slice(x);
+    return sliceArray;
+}
